e2f0af2ee9377049ecf6fec7638c3d3f1ed4e141
syam1323 <84672299+syam1323@users.noreply.github.com>
2021-06-04 11:10:40 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,10 +23,13 @@ int main(void) {
     }
   }
 }
-float wallis_pi(int a) 
-{
-	float prd=1.0;
-	for(int i=1; i<=a; i++) 
-  {prd*= (4.0*i*i)/(float)((4.0*i*i)-1);}
-	return prd*2;
-}
+float wallis_pi(int a)
+ {	float pi;
+ 	float prd=1;
+ 	for(int i=1; i<=a; i++){
+ 	float num = (4.0*i*i)/((4.0*i*i)-1);
+ 	prd= prd*num;
+ 	}
+ 	pi= 2*prd;
+ 	return pi;
+ }
